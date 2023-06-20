614d53e75ea8c65a7752248ad8a379fd6303524e
Kirill Osterov <43790130+Ziviy@users.noreply.github.com>
2023-06-20 22:53:39 +0300

    some small changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,15 @@ import Layout from './layout'
 export default function Home() {
   return (
     <>
-      <div className='posts w-100 rounded-[8px] bg-mainColor border p-2'>
+    <div className='flex justify-center gap-10 w-full'>
+    <div className='posts w-100 rounded-[8px] bg-mainColor border p-2 w-2/4'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem officiis assumenda eveniet voluptatem. Fuga doloribus cupiditate corrupti, delectus quas ipsum tempore, voluptatem optio facilis mollitia sequi fugiat exercitationem? Nulla, nesciunt?
       </div>
+      <div className='posts w-100 rounded-[8px] bg-mainColor border p-2 w-2/4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem officiis assumenda eveniet voluptatem. Fuga doloribus cupiditate corrupti, delectus quas ipsum tempore, voluptatem optio facilis mollitia sequi fugiat exercitationem? Nulla, nesciunt?
+      </div>
+    </div>
+
     </>
   )
 }
